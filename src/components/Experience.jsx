38ccdf1f464bae4fe73116688c30f9e0a4a66c21
joaovtfor <joaovtfor@hotmail.com>
2023-06-27 20:57:13 -0300

    `Add` - Mudando cor Experiências

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -72,7 +72,7 @@ const Experience = () => {
           <VerticalTimelineElement
             contentStyle={{
               background: '#eaeaec',
-              color: '#292929',
+              color: '#141414',
               boxShadow:
                 'rgba(0, 0, 0, 0.1) 0px 10px 15px -3px, rgba(0, 0, 0, 0.05) 0px 4px 6px -2px',
               display: 'flex',
