84f619c1735f50142da6fdb27397ddc8291b39e4
joaovtfor <joaovtfor@hotmail.com>
2023-06-27 19:23:22 -0300

    `Add` - Adicionando Footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,15 @@
+
+const Footer = () => {
+
+  return (
+    <footer >
+        <div className="flex justify-center align-center flex-wrap p-20 card-gradient h-7">
+            <p className="text-2xl text-eerieBlack hover:text-neutral-50 transition">
+                {`João Vitor de For dos Santos ©️ 2023. Todos os direitos reservados.`}
+            </p>
+        </div>
+    </footer>
+  );
+};
+
+export default Footer;
